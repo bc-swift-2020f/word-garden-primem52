dcaa661e4a2c1a833a93bc648e397a03b70ddd86
Morgan Prime <primem@bc.edu>
2020-09-14 12:06:39 -0400

    Initial Commit

---
+++ Word Garden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Garden
+//
+//  Created by Morgan Prime on 9/14/20.
+//  Copyright © 2020 Morgan Prime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
